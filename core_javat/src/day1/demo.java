c60079b8aec735d49ee3c3cdd39130414fc2bfc2
Nivethasubramaniyan.21 <123714345+nivetha-subramani@users.noreply.github.com>
2024-10-28 18:57:52 +0530

    . demo.java

--- core_javat/src/day1/demo.java
+++ core_javat/src/day1/demo.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 package day1;
 
 public class demo {
-	
-
+	public static void main(Strings[] args){
+		Scanner.sc = new Scanner(System.in);
+		int age = sc.nextInt();
+		System.out.println(age);
+	}
 }
